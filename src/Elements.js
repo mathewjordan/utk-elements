b6bc60a3e79318c66054ef8ab0ec72d75e7e98b9
Mat Jordan <mat@utk.edu>
2020-03-06 14:46:39 -0500

    Create react app, namespace as elements.

---
+++ src/Elements.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Elements.css';
+
+function Elements() {
+  return (
+    <div className="elements">
+      <header className="elements-header">
+      </header>
+    </div>
+  );
+}
+
+export default Elements;
