61b11f1e5615d82ffec76f5bcc16282eae568233
Mat Jordan <mat@utk.edu>
2020-03-06 15:05:27 -0500

    Build basic iterator and fetch.

--- src/Elements.js
+++ src/Elements.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Elements.css';
 
-function Elements() {
-  return (
-    <div className="elements">
-      <header className="elements-header">
-      </header>
-    </div>
-  );
+const endpoint = 'https://www.lib.utk.edu/wp-json/dc/all';
+
+class Elements extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null
+        }
+    }
+
+    componentDidMount() {
+        this.fetchElementsData()
+    }
+
+    fetchElementsData() {
+        fetch(endpoint, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    data: data.collections
+                });
+            })
+            .catch(err => console.error(this.props.url, err.toString()));
+
+        return null
+    }
+
+    iterateItems(items) {
+        return items.map((item) => (
+            <li>
+                {item.PID}
+            </li>
+        ));
+    }
+
+    render() {
+
+        let {data} = this.state
+
+        if (data)
+            return (
+                <div className="elements">
+                    <header className="elements-header">
+                        <h1>Data</h1>
+                    </header>
+                    <main>
+                        {this.iterateItems(data)}
+                    </main>
+                </div>
+            )
+        else
+            return null
+    }
 }
 
 export default Elements;
